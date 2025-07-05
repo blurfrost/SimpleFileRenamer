bebadec72d433c05eccf3a126793117185c8daae
blurfrost <ksyc07@gmail.com>
2025-07-06 00:49:39 +0800

    Added functionality to recursively search through provided directory

--- renamer.java
+++ renamer.java
@@ -1,4 +1,7 @@
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.stream.Stream;
 import java.util.Scanner;
 
 class Renamer {
@@ -6,12 +9,19 @@ class Renamer {
         String folderName = "ToRename";
         File folder = new File(Renamer.class.getResource(folderName).getPath());
         System.out.println("Folder Path: " + folder.getAbsolutePath());
+        Path folderToRenameFrom = Paths.get(folder.toString());
 
         Scanner confirmScanner = new Scanner(System.in);
         System.out.println("Renaming contents of folder " + folderName + ", input Y to confirm:");
         String input = confirmScanner.nextLine();
         if (input.equals("Y")) {
-            System.out.println("processing");
+            System.out.println("Processing...");
+            try (Stream<Path> stream = Files.walk(folderToRenameFrom)) {
+                stream.filter(Files::isRegularFile)
+                    .forEach(file -> System.out.println(file.getFileName()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         } else {
             System.out.println("Aborting operation");
         }
